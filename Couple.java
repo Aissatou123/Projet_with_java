b7ad8fb7c11fd7e55ec86f45b1fea383dae21a47
Aissatou123 <78451829+Aissatou123@users.noreply.github.com>
2021-02-06 02:16:09 +0100

    Update Couple.java

--- Couple.java
+++ Couple.java
@@ -1,2 +1,14 @@
 public class Couple {
+  private int membre1,membre2;
+  public Couple (int p, int q) {
+
+membre1 = p;
+membre2 = q;
+}
+  public Couple (int p, int q) {
+
+membre1= 0;
+membre2 = 0;
+}
+
 }
