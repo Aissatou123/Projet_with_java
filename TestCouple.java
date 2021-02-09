343448b094e698ac30eb1c92e31e92f0b2207801
Aissatou123 <78451829+Aissatou123@users.noreply.github.com>
2021-02-09 21:54:07 +0100

    Update TestCouple.java

--- TestCouple.java
+++ TestCouple.java
@@ -1,2 +1,14 @@
 public class TestCouple{
+	public static void main(String[] args){
+		Couple c1=new Couple(2,3);
+		Couple c2= new Couple(0,0);
+		Couple c3= new Couple(2,3);
+		c1.display();
+		c2.display();
+		c3.display();
+			c3.setP(8);
+		    System.out.println("Après modification, les éléments de c3 sont: "+c3.getP()+","+c3.getQ());
+		    compare(c1,c3); //ERREUR ICI
+		    System.out.println("c1 inférieur à c3: "+comparé);
+	}
 }
