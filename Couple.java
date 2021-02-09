2ffe834afc2ab648ccdcc7d0b4cd1a867f2ed9e8
Aissatou123 <78451829+Aissatou123@users.noreply.github.com>
2021-02-09 21:53:06 +0100

    Update Couple.java

--- Couple.java
+++ Couple.java
@@ -1,3 +1,4 @@
+//REVOIR TOUT
 public class Couple {
   private int p,q;
   public Couple(int membre1, int membre2) {
@@ -5,12 +6,13 @@ public class Couple {
 p = membre1;
 q= membre2;
 }
-  public Couple(int a) {
-a=0;
-p = a;
-p = a;
+public Couple() {
+p= 0;
+q= 0;
 }
-  public Couple () {
+public Couple (Couple x) {
+
+
 }
 public int getP() {
 return p;
@@ -20,22 +22,23 @@ return q;
 }
 public void setP(int c){
 	p=c;
+
 }
 public void setQ(int d){
 	q=d;
+	
 }
 void display(){
 	System.out.println("("+p+","+q+")");
 }
- public boolean compare(Couple){
+ public boolean compare(Couple alpha, Couple beta){
+
 	//(p1, q1) < (p2, q2) si et seulement si (p1 < p2) ou (p1 = p2 et q1 < q2).//
-	
-	if (p1<p2 || p1==p2 && q1<q2) {
-		Couple alpha=new Couple(p1,q1);
-		Couple beta=new Couple(p2,q2);
-		return(compare(alpha,beta)); //num1<num2
+	if (alpha.p<beta.p || alpha.p==beta.p && alpha.q<beta.q) {
+		 return(true); //num1<num2
 
+}else{
+	return(false);
 }
 }
-
 }
