30f50311d068990ff3614d0580553a42de2e2109
Aissatou123 <78451829+Aissatou123@users.noreply.github.com>
2021-02-13 17:59:50 +0100

    Update Couple.java

--- Couple.java
+++ Couple.java
@@ -1,18 +1,24 @@
+//la classe couple
 public class Couple {
+	//variables//
   private int p,q;
+	//constructeur qui initialise les variables//
   public Couple(int membre1, int membre2) {
 
 p = membre1;
 q= membre2;
 }
+	//constructeur qui initialise a zéro les variables//
 public Couple() {
 p= 0;
 q= 0;
 }
+	//constructeur qui initialise un couple a l'aide d'un autre couple//
 public Couple (Couple x) {
 
 
 }
+	//accesseurs//
 public int getP() {
 return p;
 }
@@ -27,10 +33,10 @@ public void setQ(int d){
 	q=d;
 	
 }
-void display(){
+void display(){ // méthode qui affiche les variables d’instance
 	System.out.println("("+p+","+q+")");
 }
- public boolean compare(Couple alpha, Couple beta){
+ public boolean compare(Couple alpha, Couple beta){ //méthode qui compare deux couples
 
 	//(p1, q1) < (p2, q2) si et seulement si (p1 < p2) ou (p1 = p2 et q1 < q2).//
 	if (alpha.p<beta.p || alpha.p==beta.p && alpha.q<beta.q) {
